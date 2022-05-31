b6539b4c3ec3cb0f801287bcf9573911102afed4
Ezra Peisach <ezra.peisach@rcsb.org>
2022-05-31 12:57:20 -0400

    v0.2 release

--- onedep_biocuration/__init__.py
+++ onedep_biocuration/__init__.py
@@ -2,6 +2,6 @@ __docformat__ = "restructuredtext en"
 __author__ = "John Westbrook"
 __email__ = "john.westbrook@rcsb.org"
 __license__ = "Apache 2.0"
-__version__ = "0.1"
+__version__ = "0.2"
 
 __apiUrl__ = "https://da-devel-1-ann.rcsb.rutgers.edu"
