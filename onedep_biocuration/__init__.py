3528dec7c8d7c1f6163baecab3c83457a7bebff6
Ezra Peisach <ezra.peisach@rcsb.org>
2023-05-11 08:31:57 -0400

    v0.3

--- onedep_biocuration/__init__.py
+++ onedep_biocuration/__init__.py
@@ -2,6 +2,6 @@ __docformat__ = "restructuredtext en"
 __author__ = "John Westbrook"
 __email__ = "john.westbrook@rcsb.org"
 __license__ = "Apache 2.0"
-__version__ = "0.2"
+__version__ = "0.3"
 
 __apiUrl__ = "https://onedep-contentws-rcsb.wwpdb.org"
